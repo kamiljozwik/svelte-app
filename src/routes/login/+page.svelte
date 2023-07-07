7054855b5ffbbb2e313e7b285617fe88e0ebba3b
Kamil Jozwik <kamiljozwikpl@gmail.com>
2023-07-07 19:27:32 +0200

    add login with Google

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,15 @@
 <script lang="ts">
+	import { auth } from '$lib/firebase';
+	import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+
+	async function signInWithGoogle() {
+		// This auth provides only JWT to the FE (which is OK with Firebase SDK), but wont allow us to check auth status on the server. On server we can use cookies.
+		const provider = new GoogleAuthProvider();
+		const user = await signInWithPopup(auth, provider);
+		console.log(user);
+	}
 </script>
 
 <h2>Login</h2>
+
+<button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
