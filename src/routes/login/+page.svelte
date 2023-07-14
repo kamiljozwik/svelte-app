086cb25520c3325e05778622a15e80f2e72ca8f8
Kamil Jozwik <kamiljozwikpl@gmail.com>
2023-07-14 16:12:11 +0200

    cookie-based auth

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,8 +5,23 @@
 	async function signInWithGoogle() {
 		// This auth provides only JWT to the FE (which is OK with Firebase SDK), but wont allow us to check auth status on the server. On server we can use cookies.
 		const provider = new GoogleAuthProvider();
-		const user = await signInWithPopup(auth, provider);
-		console.log(user);
+		const credential = await signInWithPopup(auth, provider); // user data
+
+		const idToken = await credential.user.getIdToken();
+
+		const res = await fetch('/api/signin', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+				// 'CSRF-Token': csrfToken  // HANDLED by sveltekit automatically
+			},
+			body: JSON.stringify({ idToken })
+		});
+	}
+
+	async function signOutSSR() {
+		const res = await fetch('/api/signin', { method: 'DELETE' });
+		await signOut(auth);
 	}
 </script>
 
@@ -15,7 +30,7 @@
 {#if $user}
 	<h2 class="card-title">Welcome, {$user.displayName}</h2>
 	<p class="text-center text-success">You are logged in</p>
-	<button class="btn btn-warning" on:click={() => signOut(auth)}>Sign out</button>
+	<button class="btn btn-warning" on:click={signOutSSR}>Sign out</button>
 {:else}
 	<button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
 {/if}
